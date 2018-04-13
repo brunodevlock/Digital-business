da61f07afadd1aed1ab5b4216e10cb4982850d37
logonrm <logonrm@fiap.com.br>
2018-04-12 22:50:52 -0300

    JSF

---
+++ Aula08/10-JSF/src/br/com/fiap/jsf/bean/ProdutoBean.java
@@ -0,0 +1,32 @@
+package br.com.fiap.jsf.bean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+import br.com.fiap.ws.to.Produto;
+
+@ManagedBean
+public class ProdutoBean {
+	
+	private Produto produto;
+	
+	//Método que é acionado quando a tela é criada ou após ela ser construida
+	@PostConstruct
+	private void init() {
+		
+		produto = new Produto();
+		
+	}
+	
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+	
+
+}
